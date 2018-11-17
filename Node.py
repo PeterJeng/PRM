a709212a0504c68f8e45c84c745f1ba399959a6d
PeterJeng <peterjeng1@gmail.com>
2018-11-16 19:30:44 -0500

    Added nearest neighbors and node class. Continued with PRM algo

--- Node.py
+++ Node.py
@@ -1,5 +1,73 @@
-class Node:
+import nearest_neighbors as nn
 
-    def __init__(self, translation_matrix, rotation_matrix):
-        self.RotationMatrix = []
-        self.TranslationMatrix = []
+
+# class that defines a node in se(3)
+class RelativePosition:
+    def __init__(self, translation, rotation):
+        # need 7 dimensional node because of quaternion definition
+
+        self.translation = translation
+        self.rotation = rotation
+
+
+# a vertex with neighbors
+class Node(RelativePosition):
+    def __init__(self, translation, rotation):
+        RelativePosition.__init__(self, translation, rotation)
+        self.neighbors = nn.pad_or_truncate([], nn.INIT_CAP_NEIGHBORS, -1)
+        self.nr_neighbors = 0
+        self.added_index = 0
+        self.index = 0
+        self.cap_neighbors = nn.INIT_CAP_NEIGHBORS
+
+    def getX(self):
+        return self.translation[0][3]
+
+    def getY(self):
+        return self.translation[1][3]
+
+    def getZ(self):
+        return self.translation[2][3]
+
+    def set_index(self, index):
+        self.index = index
+
+    def get_index(self):
+        return self.index
+
+    def get_neighbors(self):
+        return self.nr_neighbors, self.neighbors
+
+    def add_neighbor(self, nd):
+        if self.nr_neighbors >= self.cap_neighbors - 1:
+            self.cap_neighbors = 2 * self.cap_neighbors
+            self.neighbors = nn.pad_or_truncate(self.neighbors, self.cap_neighbors, -1)
+        self.neighbors[self.nr_neighbors] = nd
+        self.nr_neighbors += 1
+
+    def delete_neighbor(self, nd):
+        index = 0
+        for x in range(0, self.nr_neighbors):
+            if self.neighbors[index] == nd:
+                break
+            index += 1
+        if index >= self.nr_neighbors:
+            return
+
+        self.neighbors[index] = None
+
+        self.neighbors[index], self.neighbors[self.nr_neighbors - 1] \
+            = self.neighbors[self.nr_neighbors - 1], self.neighbors[index]
+        self.nr_neighbors -= 1
+
+    def replace_neighbor(self, prev, new_index):
+        index = 0
+        for x in range(0, self.nr_neighbors):
+            if self.neighbors[index] == prev:
+                break
+            index += 1
+        if index >= self.nr_neighbors:
+            return
+        self.neighbors[index] = new_index
+
+
--- PRMAlgo.py
+++ PRMAlgo.py
@@ -1,18 +1,18 @@
 from Node import Node
 from random import random
-from math import sqrt, pi, cos, sin
+from math import sqrt, pi, cos, sin, acos
 import numpy as np
 import quaternion
+from scipy.spatial import distance
 
 # import rospy
 
 edge_list = []
-vertex_list = []
+vertex_list = {}
 
 
 def generate_quaternion():
-
-    s = random()
+    s = random.uniform(0, 1)
     sig_1 = sqrt(1 - s)
     sig_2 = sqrt(s)
     angle_1 = 2 * pi * random()
@@ -28,11 +28,14 @@ def generate_quaternion():
 # Compute the quaternion inner product lambda
 # Return a range between [-1,1]
 def quaternion_distance(q1, q2):
-    q3 = q1 * q2
+    first = acos((q1.w * q2.w) + (q1.x * q2.x) + (q1.y * q2.y) + (q1.z * q2.z))
+    second = acos((q1.w * -q2.w) + (q1.x * -q2.x) + (q1.y * -q2.y) + (q1.z * -q2.z))
+
+    return min(first, second)
 
 
-def sample_point():
-    print()
+def translation_distance(t1, t2):
+    return distance.euclidean(t1, t2)
 
 
 def collision_free_configuration(translation_matrix, rotation_matrix):
@@ -48,7 +51,8 @@ def collision_free_configuration(translation_matrix, rotation_matrix):
         print("Service Call Failed: %s", e)
 
 
-def neighborhood(q, Vertex, Edge):
+def neighborhood(q, vertex, edge):
+
     return [1, 2, 3]
 
 
@@ -63,24 +67,61 @@ def collision_free_path(path):
     return True
 
 
+def slerp(v0, v1, t_array):
+    # >>> slerp([1,0,0,0],[0,0,0,1],np.arange(0,1,0.001))
+    t_array = np.array(t_array)
+    v0 = np.array(v0)
+    v1 = np.array(v1)
+    dot = np.sum(v0 * v1)
+
+    if dot < 0.0:
+        v1 = -v1
+        dot = -dot
+
+    DOT_THRESHOLD = 0.9995
+    if dot > DOT_THRESHOLD:
+        result = v0[np.newaxis, :] + t_array[:, np.newaxis] * (v1 - v0)[np.newaxis, :]
+        result = result / np.linalg.norm(result)
+        return result
+
+    theta_0 = np.arccos(dot)
+    sin_theta_0 = np.sin(theta_0)
+
+    theta = theta_0 * t_array
+    sin_theta = np.sin(theta)
+
+    s0 = np.cos(theta) - dot * sin_theta / sin_theta_0
+    s1 = sin_theta / sin_theta_0
+    return (s0[:, np.newaxis] * v0[np.newaxis, :]) + (s1[:, np.newaxis] * v1[np.newaxis, :])
+
+
 if __name__ == "__main__":
     iteration = int(input("How many iterations?"))
 
     for n in range(iteration):
         print("Iteration: " + str(n + 1))
 
-        collision_check = True
+        good_sample = False
         translation_matrix = []
         rotation_matrix = []
 
-        # while collision_check:
-            # generate a translation matrix
-            # generate a quaternion, use function to convert to rotation matrix
+        while not good_sample:
+            # generate a random transformation matrix inside the world space
+            t_matrix = [random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(0, 5)]
+
+            # generate a random quaternion
+            r_matrix = quaternion.as_rotation_matrix(generate_quaternion())
+
+            if collision_free_configuration(t_matrix, r_matrix) == 0:
+                good_sample = True
+                translation_matrix = t_matrix
+                rotation_matrix = r_matrix
 
         current_node = Node(translation_matrix, rotation_matrix)
 
         # add node to vertex_list, might need to build this differently
-        vertex_list.append(current_node)
+        if current_node not in vertex_list:
+            vertex_list.append(current_node)
 
         # create a neighborhood of nodes given a node
         neighborhood_list = neighborhood(current_node, vertex_list, edge_list)
